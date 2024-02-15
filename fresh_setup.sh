13b3c7c4b21eec90730b34bf7cc7c5df742f82bb
Gabe WSL Debian <24978329+EarthenSky@users.noreply.github.com>
2024-02-15 14:58:58 -0800

    add python requirements & start fresh setup script

--- fresh_setup.sh
+++ fresh_setup.sh
@@ -1,3 +1,46 @@
 # this is a script for seting up the website from a fresh install
 
-# TODO: this
+echo "hi sysadmin!"
+echo "this script will install everything needed to run the csss website"
+echo "==="
+
+echo "update apt to latest packages..."
+sudo apt update && sudo apt upgrade -y
+
+# TODO: look into `sudo apt install unattended-upgrades`
+# TODO: look into activating fail2ban for ssh protection (I doubt we'll need this unless we get too much random traffic)
+
+echo "installing git..."
+sudo apt install git
+
+echo "creating csss_site user account..."
+sudo useradd csss-site -m # has home
+sudo usermod -L csss-site # cannot login
+
+echo "installing python3.12"
+#sudo apt-get install python3-launchpadlib
+sudo apt-get install software-properties-common
+#sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+#sudo apt install python3.12 python3.12-venv -y
+sudo apt install python3.11 python3.11-venv -y # default on debian 12
+
+echo "installing supervisor & nginx"
+sudo apt install supervisor nginx -y
+
+echo "enable & start supervisor"
+sudo systemctl enable supervisor
+sudo systemctl start supervisor
+
+echo "clone csss-site backend"
+git clone git@github.com:CSSS/csss-site-backend.git
+
+echo "creating a virtual environment for python"
+cd /home/csss-site
+python3.11 -m venv .venv
+source .venv/bin/activate
+
+echo "installing pip packages"
+cd csss-site-backend
+python3.11 -m pip install -r requirements.txt
+

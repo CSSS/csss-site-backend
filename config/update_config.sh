1d9465f5ac8fcc446d84e1992ed3996b08ef9969
Gabe WSL Debian <24978329+EarthenSky@users.noreply.github.com>
2024-02-17 12:08:29 -0800

    replace supervisor with systemd

--- config/update_config.sh
+++ config/update_config.sh
@@ -1,6 +1,12 @@
-echo "update nginx configs"
+#!/bin/bash
+
+echo "1. update nginx configs"
 cp config/nginx.conf /etc/nginx/sites-available/csss-site
 sudo nginx -t
 sudo systemctl restart nginx
 
-echo "update supervisor configs (TODO)"
+echo "2. update csss-site service config"
+sudo systemd-analyze verify /home/csss-site/csss-site-backend/config/csss-site.service
+cp /home/csss-site/csss-site-backend/config/csss-site.service /etc/systemd/system/csss-site.service
+sudo systemctl restart csss-site
+
--- fresh_setup.sh
+++ fresh_setup.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
+
 # this is a script for seting up the website from a fresh install
 
 echo "hi sysadmin!"
-echo "this script will install everything needed to run the csss website"
+echo "this script will install (almost) everything needed to run the csss website"
 echo "==="
 
 echo "update apt to latest packages..."
@@ -18,19 +20,12 @@ sudo useradd csss-site -m # has home
 sudo usermod -L csss-site # cannot login
 
 echo "installing python3.12"
-#sudo apt-get install python3-launchpadlib
 sudo apt-get install software-properties-common
-#sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
-#sudo apt install python3.12 python3.12-venv -y
 sudo apt install python3.11 python3.11-venv -y # default on debian 12
 
 echo "installing supervisor & nginx"
-sudo apt install supervisor nginx -y
-
-echo "enable & start supervisor"
-sudo systemctl enable supervisor
-sudo systemctl start supervisor
+sudo apt install nginx -y
 
 echo "clone csss-site backend"
 cd /home/csss-site
@@ -50,17 +45,15 @@ pushd src
 mkdir run
 popd
 
-echo "update ownership for supervisor"
+echo "update ownership"
 chown csss-site ./src -R
 chown csss-site ./gunicorn_start -R
 chgrp csss-site ./src -R
 chgrp csss-site ./gunicorn_start -R
 
-echo "configure supervisor"
-mkdir logs
-cp config/supervisor.conf /etc/supervisor/conf.d/csss-site.conf
-sudo supervisorctl reread
-sudo supervisorctl update
+cp config/csss-site.service /etc/systemd/system/csss-site.service
+systemctl start csss-site
+systemctl enable csss-site
 
 # NOTE: there was some trial & error with these permissions, they may not work first time
 echo "configure nginx"
@@ -78,4 +71,6 @@ sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
 sudo certbot --nginx #NOTE: you'll have to fill this out manually: csss-sysadmin@sfu.ca
+
+# NOTE: file permissions during this setup process needs some work
 

7941a3743a80032905ec41306833a7b3436718e7
Micah Baker <micahb923@gmail.com>
2024-02-24 15:08:41 -0800

    Update fresh_setup.sh

--- fresh_setup.sh
+++ fresh_setup.sh
@@ -1,96 +1,99 @@
 #!/bin/bash
 
+# TODO:
+# - look into `apt install unattended-upgrades`
+# - look into activating fail2ban for ssh protection (I doubt we'll need this unless we get too much random traffic)
+
 # this is a script for seting up the website from a fresh install
 
 echo "hi sysadmin!"
 echo "this script will install (almost) everything needed to run the csss website"
+echo "(make sure you are running on a Debian 12 Linux machine as the superuser!)"
 echo "==="
 
-echo "update apt to latest packages..."
-sudo apt update && sudo apt upgrade -y
+# ask the user for consent to proceed
+while true; do
+	echo "(P)roceed, (c)ancel?"
+	read choice
 
-# TODO: look into `sudo apt install unattended-upgrades`
-# TODO: look into activating fail2ban for ssh protection (I doubt we'll need this unless we get too much random traffic)
+	if [ $choice = 'P' ]; then
+		break
+	elif [ $choice = 'c' ]; then
+		exit 0
+	else
+		echo "Not sure what you mean..."
+	fi
+done
 
-echo "installing git..."
-sudo apt install git
+echo "configure apt sources..."
+echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
 
-echo "creating csss_site user account..."
-sudo useradd csss-site -m # has home
-sudo usermod -L csss-site # cannot login
+echo "update and upgrade apt..."
+apt update && apt upgrade -y
 
-echo "installing python3.12"
-sudo apt-get install software-properties-common
-sudo apt update
-sudo apt install python3.11 python3.11-venv -y # default on debian 12
+echo "install packages..."
+apt install git software-properties-common python3.11 python3.11-venv libaugeas0 nginx postgresql-15 postgresql-contrib -y
+# install certbot
+python3 -m venv /opt/certbot
+/opt/certbot/bin/pip install --upgrade pip
+/opt/certbot/bin/pip install certbot certbot-nginx
+ln -s /opt/certbot/bin/certbot /usr/bin/certbot
 
-echo "installing supervisor & nginx"
-sudo apt install nginx -y
-#chmod g=rwx /home/csss-site/csss-site-backend/src -R
+echo "add user csss_site..."
+useradd csss-site -m # -m: has home /home/csss-site
+usermod -L csss-site # -L: cannot login
 
-echo "clone csss-site backend"
+echo "clone repository csss-site-backend..."
+sudo -u csss-site git clone git@github.com:CSSS/csss-site-backend.git /home/csss-site
+
+echo "configure sudo for csss-site..."
+cp /home/csss-site/csss-site-backend/config/sudoers.conf /etc/sudoers.d/csss-site
+
+echo "configure nginx..."
+cp /home/csss-site/csss-site-backend/config/nginx.conf /etc/nginx/sites-available/csss-site
+ln -s /etc/nginx/sites-available/csss-site /etc/nginx/sites-enabled/
+echo "You'll need to fill out the certbot configuration manually."
+echo "Use csss-sysadmin@sfu.ca for contact email."
+certbot --nginx
+nginx -t
+
+echo "configure www-data user and /var/www..."
+usermod -aG www-data csss-site
+chown -R www-data:www-data /var/www
+chmod -R ug=rwx,o=rx /var/www
+
+echo "start nginx..."
+systemctl start nginx && systemctl enable nginx
+
+echo "configure postgres..."
+# see https://towardsdatascience.com/setting-up-postgresql-in-debian-based-linux-e4985b0b766f for more details
+# NOTE: the installation of postgresql-15 creates the postgres user, which has special privileges
+sudo -u postgres createdb --no-password main
+sudo -u postgres createuser --no-password csss-site
+sudo -u postgres psql --command='GRANT ALL PRIVILEGES ON DATABASE main TO "csss-site"'
+sudo -u postgres psql main --command='GRANT ALL ON SCHEMA public TO "csss-site"'
+
+echo "login to csss-site..."
+su csss-site
+# (csss-site)
 cd /home/csss-site
-git clone git@github.com:CSSS/csss-site-backend.git
 
-echo "creating a virtual environment for python"
+echo "create a virtual environment for csss-site..."
 python3.11 -m venv .venv
 source .venv/bin/activate
 
-echo "installing pip packages"
+echo "install pip packages for csss-site..."
 cd csss-site-backend
-#sudo apt install swig gcc-11
-python3.11 -m pip install -r requirements.txt
+sudo -u csss-site python3.11 -m pip install -r requirements.txt
+deactivate
 
-echo "update ownership"
-chown csss-site:csss-site ./src -R
-chown csss-site:csss-site ./gunicorn_start.sh -R
+echo "logout from csss-site..."
+exit
+# (root)
 
-echo "setup csss-site systemd service"
-cp config/csss-site.service /etc/systemd/system/csss-site.service
-systemctl start csss-site
-systemctl enable csss-site
+echo "configure csss-site systemd service..."
+cp /home/csss-site/csss-site-backend/config/csss-site.service /etc/systemd/system/csss-site.service
 
-echo "setup sudo access to nginx and csss-site"
-cp config/sudoers.conf /etc/sudoers.d/csss-site
-
-# NOTE: there was some trial & error with these permissions, they may not work first time
-echo "configure nginx"
-cp config/nginx.conf /etc/nginx/sites-available/csss-site
-sudo usermod -aG csss-site www-data
-sudo usermod -aG www-data csss-site
-sudo chown www-data:www-data /var/www
-sudo chmod g=rwx /var/www/html
-sudo chmod g=rwx /var/www/
-sudo ln -s /etc/nginx/sites-available/csss-site /etc/nginx/sites-enabled/
-sudo nginx -t
-sudo systemctl restart nginx
-
-echo "setup certbot for https"
-sudo apt install snapd
-sudo snap install core; sudo snap refresh core
-sudo snap install --classic certbot
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot --nginx #NOTE: you'll have to fill this out manually: csss-sysadmin@sfu.ca
-
-echo "install postgres"
-# get the official apt repository
-# see https://www.postgresql.org/download/linux/debian/ for more details
-sudo sh -c 'echo "deb https://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo apt-get update
-sudo apt-get -y install postgresql-15 postgresql-contrib
-
-echo "update postgres config"
-# see https://towardsdatascience.com/setting-up-postgresql-in-debian-based-linux-e4985b0b766f for more details
-sudo -i -u postgres
-createdb --no-password main
-createuser --no-password csss-site
-psql --command='GRANT ALL PRIVILEGES ON DATABASE main TO "csss-site"'
-psql main --command='GRANT ALL ON SCHEMA public TO "csss-site"'
-
-# not neccesary because it's checked into the git repo
-# echo "alembic setup"
-# alembic init --template async alembic
-
-# NOTE: file permissions during this setup process needs some work
-
+echo "start csss-site..."
+systemctl start csss-site && systemctl enable csss-site

a0f6c0b0ca5e9b6e0ce1257895976fbb0aeb6481
DerpyWasHere <seanchan@telus.net>
2024-08-03 13:57:23 +0000

    Changed comparisons to none to 'cond is None', removed whitespace as per linter.

--- src/github/github.py
+++ src/github/github.py
@@ -108,41 +108,41 @@ async def get_user_by_username(
     return [GithubUser(user["login"], user["id"], user["name"]) for user in [result_json]]
 
 async def get_user_by_id(
-    id: str    
+    uid: str    
 ) -> GithubUser:
     """
         Takes in a Github user id and returns an instance of GithubUser.
 
         May return an empty list if no such user was found.
     """
-    result = await _github_request_get(f"https://api.github.com/user/{id}",
+    result = await _github_request_get(f"https://api.github.com/user/{uid}",
                               os.environ.get("GITHUB_TOKEN"))
     result_json = result.json()
     return [GithubUser(user["login"], user["id"], user["name"]) for user in [result_json]]
-    
+
 async def add_user_to_org(
         org: str = github_org_name,
         uid: str | None = None,
         email: str | None = None
 ) -> None:
-    if uid == None and email == None:
+    if uid is None and email is None:
         raise Exception("uid and username cannot both be empty")
     result = None
     # Arbitrarily prefer uid
     if uid is not None and email is None:
-        result = await _github_request_post(f"https://api.github.com/orgs/{org}/invitations", 
-                               os.environ.get("GITHUB_TOKEN"), 
+        result = await _github_request_post(f"https://api.github.com/orgs/{org}/invitations",
+                               os.environ.get("GITHUB_TOKEN"),
                                dumps({"invitee_id":uid, "role":"direct_member"}))
     # Assume at this point both exist, but use email
     else:
-        result = await _github_request_post(f"https://api.github.com/orgs/{org}/invitations", 
-                               os.environ.get("GITHUB_TOKEN"), 
+        result = await _github_request_post(f"https://api.github.com/orgs/{org}/invitations",
+                               os.environ.get("GITHUB_TOKEN"),
                                dumps({"email":email, "role":"direct_member"}))
     result_json = result.json()
     # Logging here potentially?
     if result.status_code != 201:
-        raise Exception(f"Status code {result.status_code} returned when attempting to add user to org: {result_json['message']}: {str([error['message'] for error in result_json['errors']])}")
-    
+        raise Exception(f"Status code {result.status_code} returned when attempting to add user to org: {result_json['message']}: {[error['message'] for error in result_json['errors']]}")
+
 async def delete_user_from_org(
         username: str,
         org: str = github_org_name
@@ -154,14 +154,13 @@ async def delete_user_from_org(
     # Logging here potentially?
     if result.status_code != 204:
         raise Exception(f"Status code {result.status_code} returned when attempting to delete user {username} from organization {org}")
-    
+  
 async def get_teams(
         org: str = github_org_name
 ) -> list[str]:
     result = await _github_request_get(f"https://api.github.com/orgs/{org}/teams", os.environ.get("GITHUB_TOKEN"))
     json_result = result.json()
     return [GithubTeam(team["id"], team["url"], team["name"], team["slug"]) for team in json_result]
-
 
 async def add_user_to_team(
         username: str,
@@ -175,7 +174,7 @@ async def add_user_to_team(
     # Logging here potentially?
     if result.status_code != 200:
         raise Exception(f"Status code {result.status_code} returned when attempting to add user to team: {result_json['message']}")
-    
+
 async def remove_user_from_team(
         username: str,
         slug: str,

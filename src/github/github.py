c5192bba39f88ccc05f864510e6e747005e81b13
DerpyWasHere <seanchan@telus.net>
2024-08-03 13:29:37 +0000

    Added get_teams(), add_user_to_team(), changed if statements to be pythonic throughout file

--- src/github/github.py
+++ src/github/github.py
@@ -12,6 +12,14 @@ class GithubUser:
     id: int
     name: str
 
+@dataclass
+class GithubTeam:
+    id: int
+    url: str
+    name: str
+    # slugs are the space-free special names that github likes to use
+    slug: str
+
 async def _github_request_get(
         url: str,
         token: str
@@ -25,7 +33,7 @@ async def _github_request_get(
         }
     )
     rate_limit_remaining = int(result.headers["x-ratelimit-remaining"])
-    if(rate_limit_remaining) < 50:
+    if rate_limit_remaining < 50:
         raise Exception("Less than 50 api calls remaining before being rate limited, please try again later")
     
     return result
@@ -45,7 +53,7 @@ async def _github_request_post(
         data=post_data
     )
     rate_limit_remaining = int(result.headers["x-ratelimit-remaining"])
-    if(rate_limit_remaining) < 50:
+    if rate_limit_remaining < 50:
         raise Exception("Less than 50 api calls remaining before being rate limited, please try again later")
 
     return result
@@ -62,7 +70,26 @@ async def _github_request_delete(
             "X-GitHub-Api-Version": "2022-11-28"}
     )
     rate_limit_remaining = int(result.headers["x-ratelimit-remaining"])
-    if(rate_limit_remaining) < 50:
+    if rate_limit_remaining < 50:
+        raise Exception("Less than 50 api calls remaining before being rate limited, please try again later")
+
+    return result
+
+async def _github_request_put(
+        url: str,
+        token: str,
+        put_data: Any
+) -> Response:
+    result = requests.put(
+        url,
+        headers={
+            "Accept": "application/vnd.github+json",
+            "Authorization": f"Bearer {token}",
+            "X-GitHub-Api-Version": "2022-11-28"},
+        data=put_data
+    )
+    rate_limit_remaining = int(result.headers["x-ratelimit-remaining"])
+    if rate_limit_remaining < 50:
         raise Exception("Less than 50 api calls remaining before being rate limited, please try again later")
 
     return result
@@ -113,8 +140,8 @@ async def add_user_to_org(
                                dumps({"email":email, "role":"direct_member"}))
     result_json = result.json()
     # Logging here potentially?
-    if(result.status_code != 201):
-        raise Exception(f"{result.status_code}: {result_json['message']}: {str([error['message'] for error in result_json['errors']])}")
+    if result.status_code != 201:
+        raise Exception(f"Status code {result.status_code} returned when attempting to add user to org: {result_json['message']}: {str([error['message'] for error in result_json['errors']])}")
     
 async def delete_user_from_org(
         username: str,
@@ -125,6 +152,36 @@ async def delete_user_from_org(
     result = await _github_request_delete(f"https://api.github.com/orgs/{org}/memberships/{username}",
                                           os.environ.get("GITHUB_TOKEN"))
     # Logging here potentially?
-    if(result.status_code != 204):
+    if result.status_code != 204:
         raise Exception(f"Status code {result.status_code} returned when attempting to delete user {username}")
+    
+async def get_teams(
+        org: str = github_org_name
+) -> list[str]:
+    result = await _github_request_get(f"https://api.github.com/orgs/{org}/teams", os.environ.get("GITHUB_TOKEN"))
+    json_result = result.json()
+    return [GithubTeam(team["id"], team["url"], team["name"], team["slug"]) for team in json_result]
+
+
+async def add_user_to_team(
+        org: str,
+        username: str,
+        team_id: str | None = None,
+        slug: str | None = None
+) -> None:
+    if team_id is None and slug is None:
+        raise Exception("team_id and slug cannot both be none.")
+    result = None
+    if team_id is not None and slug is None:
+        result = await _github_request_put(f"https://api.github.com/orgs/{org}/team/{team_id}/memberships/{username}",
+                                           os.environ.get("GITHUB_TOKEN"),
+                                           dumps({"role":"member"}))
+    else:
+        result = await _github_request_put(f"https://api.github.com/orgs/{org}/teams/{slug}/memberships/{username}",
+                                           os.environ.get("GITHUB_TOKEN"),
+                                           dumps({"role":"member"}))
+    result_json = result.json()
+    # Logging here potentially?
+    if result.status_code != 200:
+        raise Exception(f"Status code {result.status_code} returned when attempting to add user to team: {result_json['message']}")
     

d452a2a29bb7e17b17a87ae451ba1674d0ff2f93
gunmack <jreedoy2002@yahoo.com>
2024-08-08 15:17:00 -0700

    created a model for blog table

---
+++ src/blog/models.py
@@ -0,0 +1,32 @@
+from constants import COMPUTING_ID_LEN
+from database import Base
+from sqlalchemy import (
+    Column, 
+    DateTime, 
+    ForeignKey, 
+    String,
+    Text 
+)
+
+# blog table
+class BlogPosts(Base):
+    # table name
+    __tablename__ = "blog_posts"
+    
+    # title of post ( meant to be an unique key but for simplicity,
+    # using already defined primay_key )
+    title = Column(String(128), primary_key=True, nullable=False)
+    
+    # computing id
+    computing_id = Column(String(COMPUTING_ID_LEN), ForeignKey=True, nullable=False)
+
+    # dates of creation and last edit
+    date_created = Column(DateTime, nullable=False)
+    last_edited = Column(DateTime, nullable=False)
+
+    # storing the html content
+    html_content = Column(Text, nullable=False)
+
+    # tags for the respective post
+    # TODO: consider implementing limits for tag size and count
+    tags = Column(String(128))

b5103cabb126012e54171fac79475c9b5c68871b
DerpyWasHere <seanchan@telus.net>
2024-08-12 08:26:20 +0000

    Added ElectionOfficer class, created has_permission() to check whether a user is a current elections officer

--- src/permission/types.py
+++ src/permission/types.py
@@ -1,6 +1,8 @@
 from datetime import UTC, datetime, timezone
 
 import database
+import elections.crud
+import officers.constants
 import officers.crud
 from data.semesters import current_semester_start, step_semesters
 
@@ -23,3 +25,18 @@ class OfficerPrivateInfo:
         cutoff_date = step_semesters(semester_start, -NUM_SEMESTERS)
 
         return most_recent_exec_term > cutoff_date
+
+class ElectionOfficer:
+    @staticmethod
+    async def has_permission(db_session: database.DBSession, computing_id: str) -> bool:
+        """
+        An current elections officer has access to all elections, prior elections officers have no access.
+        """
+        officer_terms = await officers.crud.current_executive_team(db_session, True)
+        current_election_officer = officer_terms.get(officers.constants.OfficerPosition.ElectionsOfficer.value)[0]
+        if current_election_officer is not None:
+            # no need to verify if position is election officer, we do so above
+            if current_election_officer.private_data.computing_id == computing_id and current_election_officer.is_current_officer is True:
+                return True
+
+        return False

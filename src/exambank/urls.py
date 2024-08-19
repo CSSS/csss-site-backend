35a20742d930e1bd074a5723981760c5bdbc4869
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2024-08-19 16:53:39 -0700

    add endpoint for generating pdf

--- src/exambank/urls.py
+++ src/exambank/urls.py
@@ -18,8 +18,8 @@ router = APIRouter(
 async def all_exams(
     request: Request,
     db_session: database.DBSession,
-    course_name_starts_with: str,
-    exam_title_starts_with: str,
+    course_name_starts_with: Optional[str],
+    exam_title_starts_with: Optional[str],
 ):
     # TODO: implement this
     pass
@@ -33,13 +33,36 @@ async def get_exam(
     course_name: str,
     exam_title: str,
 ):
+    session_id = request.cookies.get("session_id", None)
+    if session_id is None:
+        raise HTTPException(status_code=401)
+      
+    computing_id = await auth.crud.get_computing_id(db_session, session_id) 
+    if computing_id is None:
+        raise HTTPException(status_code=401)
+
+    # TODO: clean this checking into one function & one computing_id check
     if not await ExamBankAccess.has_permission(request):
         raise HTTPException(status_code=401, detail="user must have exam bank access permission")
-  
-    # TODO: implement this too
 
-    # TODO: get list of files in dir & find the one we're looking for
-    #if title in EXAM_BANK_DIR:
+    # TODO: store resource locations in a db table & simply look them up
 
-    #raster_pdf_from_path()
+    course_folders = [f.name for f in os.scandir(EXAM_BANK_DIR) if f.is_dir()]
+    if course_name in course_folders:
+        exams = [
+            f.name[:-4] 
+            for f in os.scandir(f"{EXAM_BANK_DIR}/{course_name}")
+            if f.is_file() and f.name.endswith(".pdf")
+        ]
+        if exam_title in exams:
+            # TODO: test this works nicely
+            exam_path = f"{EXAM_BANK_DIR}/{course_name}/{exam_title}.pdf"
+            watermark = create_watermark(computing_id, 20)
+            watermarked_pdf = apply_watermark(exam_path, watermark)
+            image_bytes = raster_pdf(watermarked_pdf)
+
+            headers = { "Content-Disposition": f"inline; filename=\"{exam_title}_{computing_id}.pdf\"" }
+            return Response(content=image_bytes, headers=headers, media_type="application/pdf")
+
+    raise HTTPException(status_code=400, detail="could not find the requested exam")
 

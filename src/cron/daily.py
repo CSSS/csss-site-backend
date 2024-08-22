73d8c4e68c6d2981ab22568f1e21f35b4b921aa7
EarthenSky <24978329+EarthenSky@users.noreply.github.com>
2024-08-21 17:30:43 -0700

    add daily cron file for updating officer permissions

---
+++ src/cron/daily.py
@@ -0,0 +1,39 @@
+"""This module gets called by cron every day"""
+
+import asyncio
+import logging
+
+from database import _db_session
+from officers.crud import officer_terms
+
+_logger = logging.getLogger(__name__)
+
+async def update_permissions():
+    db_session = _db_session()
+
+    # TODO: get current github permissions
+
+    # TODO: get current google drive permissions
+
+    one_year_ago = datetime.today() - timedelta(days=365)
+
+    # TODO: for performance, only include officers with recent end-date (1 yr)
+    all_officer_terms = await all_officer_terms(db_session)
+    for term in all_officer_terms:
+        if utils.is_active(term):
+            # TODO: if google drive permissions is not active, update them
+            # TODO: if github permissions is not active, update them
+            pass
+        elif utils.end_date <= one_year_ago:
+            # ignore old executives
+            continue
+        else:
+            # TODO: if google drive permissions are active, remove them
+            # TODO: if github permissions are active, remove them
+            pass 
+
+    _logger.info("Complete permissions update")
+
+if __name__ == "__main__":
+    asyncio.run(update_permissions())
+

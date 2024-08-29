d9b8e52eb7eba2bbb1d0758936a983bcdbb3319f
DerpyWasHere <seanchan@telus.net>
2024-08-29 20:43:41 +0000

    added elections router to main

--- src/main.py
+++ src/main.py
@@ -2,8 +2,12 @@ import logging
 
 import auth.urls
 import database
+import elections.urls
 import officers.urls
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse, RedirectResponse
 
 import tests.urls
 
@@ -15,7 +19,23 @@ app = FastAPI(lifespan=database.lifespan, title="CSSS Site Backend")
 app.include_router(auth.urls.router)
 app.include_router(officers.urls.router)
 app.include_router(tests.urls.router)
+app.include_router(elections.urls.router)
 
 @app.get("/")
 async def read_root():
     return {"message": "Hello! You might be lost, this is actually the sfucsss.org's backend api."}
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(
+        request: Request,
+        exception: RequestValidationError
+):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder(
+            {
+                "detail": exception.errors(),
+                "body": exception.body
+            }
+        )
+    )

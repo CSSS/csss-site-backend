b9e47de18e7cc316620e543bfe287c94791ae7ac
Gabe <24978329+EarthenSky@users.noreply.github.com>
2024-08-31 21:50:33 -0700

    Add Google Drive API (#81)
    * add basic google drive api usage
    
    * finish off lightweight google api module

---
+++ tests/integration/test_google.py
@@ -0,0 +1,25 @@
+from google_api import internals
+
+
+def test__list_drives():
+    """should not fail"""
+    drive_list = internals._list_shared_drives()
+    print(drive_list)
+
+    drive_id = drive_list["drives"][0]["id"]
+    print(drive_id)
+
+    permissions = internals.list_drive_permissions(drive_id)
+    print(permissions)
+
+    # NOTE: this will raise an exception if the email address is a non-google account
+    """
+    internals.create_drive_permission(
+        drive_id,
+        {
+            "type": "user",
+            "emailAddress": "tester_123591735013000019@gmail2.ca",
+            "role": "fileOrganizer",
+        }
+    )
+    """

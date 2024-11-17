2379c2b5f57c36f58ad2cd1b720c065350687e2b
Micah Baker <114637762+micahdbak@users.noreply.github.com>
2024-11-16 21:52:28 -0800

    Remove config files and create alembic workflow (#92)

--- config/cron.sh
+++ config/cron.sh
@@ -3,4 +3,3 @@
 # run the daily script at 1am every morning
 # TODO: make sure timezone is PST
 crontab -l | { cat; echo "0 1 * * * /home/csss-site/csss-site-backend/src/cron/daily.py"; } | crontab -
-

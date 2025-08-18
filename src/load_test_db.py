d696c2c9bf84824617f51384902cfd539a454f16
p-north <puneetnorth170@gmail.com>
2025-08-17 23:02:47 -0700

    fix db null value issue when resetting test database

--- src/load_test_db.py
+++ src/load_test_db.py
@@ -295,9 +295,11 @@ async def load_test_elections_data(db_session: AsyncSession):
         slug="test-election-1",
         name="test election    1",
         type="general_election",
+        
         datetime_start_nominations=datetime.now() - timedelta(days=400),
         datetime_start_voting=datetime.now() - timedelta(days=395, hours=4),
         datetime_end_voting=datetime.now() - timedelta(days=390, hours=8),
+        avaliable_positions="president,vice-president,treasurer",
         survey_link="https://youtu.be/dQw4w9WgXcQ?si=kZROi2tu-43MXPM5"
     ))
     await update_election(db_session, Election(
@@ -307,15 +309,18 @@ async def load_test_elections_data(db_session: AsyncSession):
         datetime_start_nominations=datetime.now() - timedelta(days=400),
         datetime_start_voting=datetime.now() - timedelta(days=395, hours=4),
         datetime_end_voting=datetime.now() - timedelta(days=390, hours=8),
+        avaliable_positions="president,vice-president,treasurer",  # Added this line
         survey_link="https://youtu.be/dQw4w9WgXcQ?si=kZROi2tu-43MXPM5"
     ))
     await create_election(db_session, Election(
         slug="test-election-2",
         name="test election 2",
         type="by_election",
+       
         datetime_start_nominations=datetime.now() - timedelta(days=300),
         datetime_start_voting=datetime.now() - timedelta(days=295, hours=4),
         datetime_end_voting=datetime.now() - timedelta(days=290, hours=8),
+        avaliable_positions="president,vice-president,treasurer",
         survey_link="https://youtu.be/dQw4w9WgXcQ?si=kZROi2tu-43MXPM5 (oh yeah)"
     ))
     await create_election(db_session, Election(
@@ -325,6 +330,7 @@ async def load_test_elections_data(db_session: AsyncSession):
         datetime_start_nominations=datetime.now() - timedelta(days=5),
         datetime_start_voting=datetime.now() - timedelta(days=1, hours=4),
         datetime_end_voting=datetime.now() + timedelta(days=5, hours=8),
+        avaliable_positions="president,vice-president,treasurer",
         survey_link="https://youtu.be/dQw4w9WgXcQ?si=kZROi2tu-43MXPM5"
     ))
     await create_election(db_session, Election(
@@ -334,6 +340,7 @@ async def load_test_elections_data(db_session: AsyncSession):
         datetime_start_nominations=datetime.now() + timedelta(days=5),
         datetime_start_voting=datetime.now() + timedelta(days=10, hours=4),
         datetime_end_voting=datetime.now() + timedelta(days=15, hours=8),
+        avaliable_positions="president,vice-president,treasurer",
         survey_link=None
     ))
     await db_session.commit()

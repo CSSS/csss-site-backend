fdd9c5fe4c629bda1076eef1d19d559f99109f50
Jon Andre Briones <jonbrsoftware@gmail.com>
2025-09-07 13:51:38 -0700

    fix: forces the DB to calculate the default current time

--- src/auth/tables.py
+++ src/auth/tables.py
@@ -1,7 +1,6 @@
 from datetime import datetime
-from typing import Any
 
-from sqlalchemy import DateTime, ForeignKey, String, Text
+from sqlalchemy import DateTime, ForeignKey, String, Text, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from constants import COMPUTING_ID_LEN, SESSION_ID_LEN
@@ -18,8 +17,9 @@ class UserSession(Base):
         primary_key=True,
     )
 
+    # TODO: Make all timestamps uneditable later
     # time the CAS ticket was issued
-    issue_time: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    issue_time: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
 
     session_id: Mapped[str] = mapped_column(
         String(SESSION_ID_LEN), nullable=False, unique=True
@@ -37,9 +37,8 @@ class SiteUser(Base):
     )
 
     # first and last time logged into the CSSS API
-    # note: default date (for pre-existing columns) is June 16th, 2024
-    first_logged_in: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime(2024, 6, 16))
-    last_logged_in: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime(2024, 6, 16))
+    first_logged_in: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
+    last_logged_in: Mapped[datetime] = mapped_column(DateTime, nullable=False, server_default=func.now())
 
     # optional user information for display purposes
     profile_picture_url: Mapped[str | None] = mapped_column(Text, nullable=True)

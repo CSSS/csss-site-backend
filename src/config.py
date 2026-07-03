aa8b12edb829e9665fa6268700e73304cbb9e509
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-07-02 17:19:01 -0700

    fix: `.env` in root can be in sub-directories

--- src/config.py
+++ src/config.py
@@ -1,8 +1,12 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+ENV_FILE = Path(__file__).parent.parent / ".env"
 
 
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file=ENV_FILE)
 
     environment: str
     db_port: int | None = None

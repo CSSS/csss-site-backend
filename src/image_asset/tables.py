8acf259b1725ba74fe4104399036f5ff5187b87d
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-16 13:58:36 -0700

    feat: create image_asset migration

--- src/image_asset/tables.py
+++ src/image_asset/tables.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.types import DateTime, Integer, Text
 
@@ -12,4 +13,4 @@ class ImageAssetDB(Base):
     image_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     storage_key: Mapped[str] = mapped_column(Text, unique=True)
     original_filename: Mapped[str] = mapped_column(Text)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())

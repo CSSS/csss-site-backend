45167d81f16eb04c5c094d5459d06fae3a7dd4e6
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-15 23:33:44 -0700

    feat: add environment variables for media files

--- src/config.py
+++ src/config.py
@@ -19,5 +19,8 @@ class Settings(BaseSettings):
     translink_api_key: str | None = None
     kiosk_secret: str | None = None
 
+    media_root: str | None = None
+    media_base_url: str | None = None
+
 
 settings = Settings()  # pyright: ignore[reportCallIssue]

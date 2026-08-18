9cd50eee85e41af4b7d4d33fca7c8f032343feab
Jon Andre Briones <jonbrsoftware@gmail.com>
2026-08-18 01:30:31 -0700

    fix: update allowed auth redirect possible values

--- src/config.py
+++ src/config.py
@@ -1,4 +1,6 @@
+from ctypes import Union
 from pathlib import Path
+from typing import Literal
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
@@ -8,7 +10,7 @@ ENV_FILE = Path(__file__).parent.parent / ".env"
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=ENV_FILE)
 
-    environment: str
+    environment: Literal["dev"] | Literal["prod"] | Literal["test"]
     db_port: int | None = None
 
     cookie_secure: bool
